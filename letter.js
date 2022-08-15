91077b7aec84368cfc0d71d2f8b96669b7909727
hasanimrul <mohammadhasan.imrul@gmail.com>
2022-08-16 00:24:52 +0600

    first commit

---
+++ letter.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function(){
+    document.getElementById('text').innerText = 'I Love You Too 💘' 
+})
+
+document.getElementById('ofcourse').addEventListener('click', function(){
+    document.getElementById('text').innerText = 'I Love You Soo Much Babu 💝'
+})
